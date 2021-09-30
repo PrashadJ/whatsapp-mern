770acffb3bc310ea990bc44611b918db675da949
prashadJ <prashad.joshii@radixhealth.com>
2021-09-30 16:11:50 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDo6LSK7EhQs4sFiKKT5J51FFB4dZlgPhA",
+  authDomain: "whatsappmern-32394.firebaseapp.com",
+  projectId: "whatsappmern-32394",
+  storageBucket: "whatsappmern-32394.appspot.com",
+  messagingSenderId: "558532007725",
+  appId: "1:558532007725:web:611edfa80e3678aaae25a4",
+  measurementId: "G-XFKGB7BLBP"
+};
